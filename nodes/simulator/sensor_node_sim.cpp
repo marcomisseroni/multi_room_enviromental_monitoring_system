4be296dd229f34fa195df6bec0beeea9a557d1c1
marcomisseroni <marco.misseroni@studenti.unitn.it>
2026-05-18 11:31:52 +0200

    feat: implemented mqtt part for sensor_sim

--- nodes/simulator/sensor_node_sim.cpp
+++ nodes/simulator/sensor_node_sim.cpp
@@ -1,13 +1,79 @@
 #include <iostream>
-#include "mosquitto.h"
+#include <random>
 #include "sensor_node_sim.hpp"
 
 using namespace std;
-using namespace mqtt_node;
 
-SensorNodeSim::SensorNodeSim() {
-    
-    mosquitto_lib_init();
+namespace mqtt_node {
 
+    SensorNodeSim::SensorNodeSim(float temp_mean, float hum_mean, float press_mean, float air_qual_mean, 
+                    float temp_std, float hum_std, float press_std, float air_qual_std) 
+                    : mosquittopp("sensor_sim_node") {
+
+        // Initialize mosquitto library
+        mosqpp::lib_init();
+
+        // Parameters initialization
+        _temperature_mean = temp_mean;
+        _humidity_mean = hum_mean;
+        _pressure_mean = press_mean;
+        _air_quality_mean = air_qual_mean;
+        _temperature_std = temp_std;
+        _humidity_std = hum_std;
+        _pressure_std = press_std;
+        _air_quality_std = air_qual_std;
+        _seed = 0;
+
+        // mqtt connection
+        connect("localhost", 1883, 60);
+        loop_start();
+    }
+
+    SensorNodeSim::~SensorNodeSim() {
+        disconnect();
+        loop_stop();
+    }
+
+    void SensorNodeSim::set_seed(int new_seed) {
+        _seed = new_seed;
+    }
+
+    void SensorNodeSim::generate_data() {
+        std::default_random_engine generator(_seed);
+        std::normal_distribution<float> temp_dis(_temperature_mean, _temperature_std);
+        std::normal_distribution<float> hum_dis(_humidity_mean, _humidity_std);
+        std::normal_distribution<float> press_dis(_pressure_mean, _pressure_std);
+        std::normal_distribution<float> air_q_dis(_air_quality_mean, _air_quality_std);
+
+        float temp_reading = temp_dis(generator);
+        float hum_reading = hum_dis(generator);
+        float press_reading = press_dis(generator);
+        float air_q_reading = air_q_dis(generator);
+
+        publish_data(temp_reading, hum_reading, press_reading, air_q_reading);
+    }
+
+    int SensorNodeSim::publish_data(float temp, float hum, float press, float air_q) {
+
+        string temp_payload = to_string(temp);
+        string hum_payload = to_string(hum);
+        string press_payload = to_string(press);
+        string air_q_payload = to_string(air_q);
+
+        publish(nullptr, _temp_topic, temp_payload.size(), temp_payload.c_str());
+        publish(nullptr, _hum_topic, hum_payload.size(), hum_payload.c_str());
+        publish(nullptr, _press_topic,press_payload.size(), press_payload.c_str());
+        publish(nullptr, _air_q_topic, air_q_payload.size(), air_q_payload.c_str());
+
+        // handle mqtt errors
+        return 0;
+    }
+
+    void SensorNodeSim::on_connect(int rc) {
+        cerr << "Connected to broker" << endl;
+    }
+
+    void SensorNodeSim::on_disconnect(int rc) {
+        cerr << "Disconneted" << endl;
+    }
 }
-
--- nodes/simulator/sensor_node_sim.hpp
+++ nodes/simulator/sensor_node_sim.hpp
@@ -2,18 +2,40 @@
 #ifndef SENSOR_NODE_SIM_HPP
 #define SENSOR_NODE_SIM_HPP
 
+#include <mosquitto/libmosquittopp.h>
+
 namespace mqtt_node {
 
-    class SensorNodeSim {
+    class SensorNodeSim : public mosqpp::mosquittopp {
 
         public:
 
-            SensorNodeSim();
-            int publish_data();
+            SensorNodeSim(float temp_mean = 20, float hum_mean = 40, float press_mean = 1013, float air_qual_mean = 100, 
+                float temp_std = 2, float hum_std = 10, float press_std = 20, float air_qual_std = 20);
+            ~SensorNodeSim();
+
+            void set_seed(int new_seed);
 
         private:
 
+            float _temperature_mean;
+            float _humidity_mean;
+            float _pressure_mean;
+            float _air_quality_mean;
+            float _temperature_std;
+            float _humidity_std;
+            float _pressure_std;
+            float _air_quality_std;
+            int _seed;
+            const char *_temp_topic = "temperature_sensor";
+            const char *_hum_topic = "humidity_sensor";
+            const char *_press_topic = "pressure_sensor";
+            const char *_air_q_topic = "air_quality_sensor";
 
+            void generate_data();
+            int publish_data(float temp, float hum, float press, float air_q);
+            void on_connect(int rc) override;
+            void on_disconnect(int rc) override;
 
     };
 
